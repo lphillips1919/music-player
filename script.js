ac94ae3024dcd4d3b66db85c3e3c4a80a02b9df9
PaperScribe <ao193579@pennwest.edu>
2024-03-30 09:46:25 -0400

    Is not integrated with Trevor's queue yet

--- script.js
+++ script.js
@@ -22,20 +22,6 @@ let updateTimer;
 
 let curr_track = document.createElement('audio');
 
-// Some variables that Trevor was playing around with. We will hopefully be able to implement this better at a later date. I don't know Javascript!
-let queue_index = 0;
-
-function addToQueue(addedID) {
-    // Adds a song to the queue when the + button is clicked. If the queue is currently empty, starts playing the music.
-    if (queue_list.length == 0) {
-        loadTrack(addedID);
-    }
-    queue_list.push(addedID);
-    if (queue_list.length > 3) {
-        console.log(queue_list[queue_list.length - 1]);
-    }
-}
-
 // Define the tracks that have to be played
 let track_list = [
     {
@@ -87,32 +73,29 @@ let track_list = [
         path: "Assets/Audio/1st Contact - Der Weg.mp3"
     },
     {
-        name: "Cover Gurl",
-        artist: "Dvr Wvg",
-        image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
-        path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3"
+        name: "Night Owl",
+        artist: "Broke For Free",
+        image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+        path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3"
     },
 ];
 
-let queue_list = [];
-
-function loadTrack(song_id) {
+function loadTrack(track_index) {
     /* This function is very confusing as I found it on GeeksForGeeks. It appears to load a track when the URL to the webpage is opened and has no call in the HTML file however if its not broken don't fix it. */
-    // Trevor here, I have hijacked this function to now operate on the queue instead of the hardcoded track_list
 
     // Clear the previous seek timer
     clearInterval(updateTimer);
     resetValues();
     
     // Load a new track
-    curr_track.src = track_list[song_id].path;
+    curr_track.src = track_list[track_index].path;
     curr_track.load();
     
     // Update details of the track
     track_art.style.backgroundImage = 
-        "url(" + track_list[song_id].image + ")";
-    track_name.textContent = track_list[song_id].name;
-    track_artist.textContent = track_list[song_id].artist;
+        "url(" + track_list[track_index].image + ")";
+    track_name.textContent = track_list[track_index].name;
+    track_artist.textContent = track_list[track_index].artist;
     
     // Set an interval of 1000 milliseconds
     // for updating the seek slider
@@ -158,30 +141,25 @@ function pauseTrack() {
 function nextTrack() {
     // Go back to the first track if the
     // current one is the last in the track list
-    if (queue_list.length != 0) {
-        if (queue_index < queue_list.length - 1) {
-            queue_index += 1;
-        }
-        else queue_index = 0;
+    if (track_index < track_list.length - 1)
+        track_index += 1;
+    else track_index = 0;
 
-        // Load and play the new track
-        loadTrack(queue_list[queue_index]);
-        playTrack();
-    }
+    // Load and play the new track
+    loadTrack(track_index);
+    playTrack();
 }
 
 function prevTrack() {
     // Go back to the last track if the
     // current one is the first in the track list
-    if (queue_list.length != 0) {
-        if (queue_index > 0)
-            queue_index -= 1;
-        else queue_index = queue_list.length - 1;
+    if (track_index > 0)
+        track_index -= 1;
+    else track_index = track_list.length - 1;
 
-        // Load and play the new track
-        loadTrack(queue_list[queue_index]);
-        playTrack();
-    }
+    // Load and play the new track
+    loadTrack(track_index);
+    playTrack();
 }
 
 function seekTo() {
@@ -223,19 +201,16 @@ function seekUpdate() {
 
 // Fisher-Yates shuffle algorithm to shuffle tracks
 function shuffleTracks() {
-    if (queue_list.length != 0) {
-        for (let i = queue_list.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            const temp = queue_list[i];
-            queue_list[i] = queue_list[j];
-            queue_list[j] = temp;
-        }
-        queue_index = 0;
-        // If I didnt add the or statement then it wont be shuffled unless actively being played
-        if (isPlaying || !isPlaying) {
-            loadTrack(queue_list[queue_index]);
-            playTrack();
-        }
+    for (let i = track_list.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = track_list[i];
+        track_list[i] = track_list[j];
+        track_list[j] = temp;
+    }
+    // If I didnt add the or statement then it wont be shuffled unless actively being played
+    if (isPlaying || !isPlaying) {
+        loadTrack(track_index);
+        playTrack();
     }
 }
 
@@ -268,12 +243,28 @@ function unLoopTrack() {
 document.addEventListener("DOMContentLoaded", function () {
     // Select all the cards and their elements
     let cards = document.querySelectorAll(".card");
+    let container = document.querySelector(".card-container")
+    console.log(container);
 
     // Loop through each card
-    cards.forEach((card, index) => {
+    /*cards.forEach((card, index) => {
         // Set the initial track details for each card
         let currentTrack = track_list[index];
         updateCard(card, currentTrack);
+        console.log(card.name);
+
+    });*/
+
+    track_list.forEach(song => {
+        const songCard = `                    
+        <div class="card" id="playCard1">
+            <img src="${song.image}" class="card-img">
+            <p class="card-title" id="songTitle">${song.name}</p>
+            <p class="card-info" id="songInfo">Artist: ${song.artist}</p>
+            <button class="badge play-button" id="playButton">Play</button>
+        </div>`
+
+        container.innerHTML += songCard;
     });
 
     // Function to update the card content
@@ -288,29 +279,5 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-
-const handleClick = (event) => {
-    const listTitle = event.target;
-    const innerList = listTitle.nextElementSibling;
-    const content = innerList.querySelector(".content");
-  
-    // listTitle.classList.toggle("active");
-    if (listTitle.classList.contains("active")) {
-      listTitle.classList.remove("active");
-      innerList.style.height = 0;
-    } else {
-      listTitle.classList.add("active");
-      innerList.style.height = `${content.clientHeight}px`;
-    }
-};
-  
-const listTitles = document.querySelectorAll(".list-title");
-
-for (let listTitle of listTitles) {
-    listTitle.addEventListener("click", handleClick);
-}
-  
-
 // Load the first track in the tracklist
-//loadTrack(track_index);
-//console.log(queue_list.length);
+loadTrack(track_index);
